6bd9ada89a968dd1aa38956651d28c47f1f51404
Tracey <145255568+Traceeey@users.noreply.github.com>
2025-03-26 16:15:37 +0300

    Delete index.py

--- index.py
+++
@@ -1,8 +0,0 @@
-user_name = input("Enter your name: ")  # User name
-favourite_color = input("Enter your favourite colour: ")  # Favourite color
-favourite_food = input("Enter your favourite food: ")  # Favourite food
-print(f"Hello, {user_name}! Your favourite color is {favourite_color} and your favourite food is {favourite_food}.")
-
-
-x= {456, 5678, "Tracey", 90.65}
-print(2)
